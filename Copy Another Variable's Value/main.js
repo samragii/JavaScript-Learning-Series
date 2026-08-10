f818c76a8f017d853fa83e64db7d48c7a2f5fe7e
samragii <samragineupane7@gmail.com>
2026-08-10 22:27:01 +0545

    Copy Another Variable's Value

---
+++ Copy Another Variable's Value/main.js
@@ -0,0 +1,14 @@
+// Create a variable favorite with the value "steak"
+let favorite="steak";
+
+// Display favorite
+console.log(favorite);
+
+// Create a variable food with the value "pizza"
+let food="pizza";
+
+// Assign food to favorite
+favourite=food;
+
+// Display favorite again
+console.log(favourite);
