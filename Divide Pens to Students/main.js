aa33c7a5f9ce0448da5107d99818028f0cc5fdc2
samragii <samragineupane7@gmail.com>
2026-08-12 22:28:28 +0545

    Divide Pens to Students

---
+++ Divide Pens to Students/main.js
@@ -0,0 +1,11 @@
+// Create pen_number and student_number variables
+let pen_number=14;
+let student_number=3;
+
+// Compute quotient
+let quotient=Math.floor(pen_number/student_number);
+console.log(quotient);
+
+// Compute remainder
+let reminder=pen_number%student_number;
+console.log(reminder);
