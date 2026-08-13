8f64fad870e74cbaba286ae9d2ef834f1a68928f
samragii <samragineupane7@gmail.com>
2026-08-13 22:57:13 +0545

    Convert Type

---
+++ Convert Type/main.js
@@ -0,0 +1,10 @@
+// Create variables x and y
+let x="5";
+let y="10";
+
+// Convert values to integers and multiply them
+let a=parseInt(x);
+let b=parseInt(y);
+let muntiply=a*b;
+// Print the result
+console.log(muntiply);
