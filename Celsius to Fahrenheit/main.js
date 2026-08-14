9edb5de3af7cc54ac11dbb2ca1c1cb725a13a677
samragii <samragineupane7@gmail.com>
2026-08-14 21:45:51 +0545

    Celsius to Fahrenheit

---
+++ Celsius to Fahrenheit/main.js
@@ -0,0 +1,8 @@
+// Get temperature in degree Celsius from the user
+let Celsius=parseFloat(prompt("Enter the temperature"));
+
+// Convert it to degree Fahrenheit using the formula
+let fahrenheit=(Celsius*1.8)+32;
+
+// Print the degree in Fahrenheit
+console.log(fahrenheit);
