f425ac887fe2604b00672538809112a1746db625
samragii <samragineupane7@gmail.com>
2026-08-14 21:28:25 +0545

    Receive User Input

---
+++ Receive User Input/main.js
@@ -0,0 +1,9 @@
+// Get the first name from the user
+let firstName = prompt("Enter your first name:");
+
+// Get the last name from the user
+let lastName = prompt("Enter your last name:");
+
+// Print the full name
+console.log("First Name:",firstName);
+console.log("Last Name:",lastName);
