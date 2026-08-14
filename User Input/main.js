b8750b72cf755c7da291ecb33906685d9f970277
samragii <samragineupane7@gmail.com>
2026-08-14 21:43:05 +0545

    User Input

---
+++ User Input/main.js
@@ -0,0 +1,8 @@
+// Get two floating-point numbers as inputs
+let number1=parseFloat(prompt("Enter a number:"));
+let number2=parseFloat(prompt("Enter a number:"));
+// Add the numbers
+let add=number1+number2;
+
+// Print the square of the result
+console.log(add**2);
