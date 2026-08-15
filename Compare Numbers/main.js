fa533ac921e1d404eec1898750254a51b3cc81fb
samragii <samragineupane7@gmail.com>
2026-08-15 22:49:53 +0545

    Compare Numbers

---
+++ Compare Numbers/main.js
@@ -0,0 +1,8 @@
+// Get two integer inputs
+let var1=parseInt(prompt("Enter the variable:"));
+let var2=parseInt(prompt("Enter the variable:"));
+// Compare variables and print them in the given format
+
+console.log("var1 < var2:",var1<var2);
+console.log("var1 > var2:",var1>var2);
+console.log("var1 != var2:",var1!=var2);
