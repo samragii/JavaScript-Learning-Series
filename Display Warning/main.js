c72b97d573068b3d8f649046fdec6ffea0941c18
samragii <samragineupane7@gmail.com>
2026-08-16 20:55:12 +0545

    Display Warning

---
+++ Display Warning/main.js
@@ -0,0 +1,9 @@
+// Get input
+let traffic_signal = prompt("Enter the traffic light color:");
+
+if (traffic_signal=="red")
+{
+    console.log("The red light is on.");
+    console.log("Stop the vehicle.");
+}
+console.log("Have a good day!");
