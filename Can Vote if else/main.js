8dbdd5dcb7bd0a5d652ebea139a1e5c3f7c6f5f2
samragii <samragineupane7@gmail.com>
2026-08-17 21:56:21 +0545

    Can vote using if else

---
+++ Can Vote if else/main.js
@@ -0,0 +1,9 @@
+// Get integer input from the user
+let age = Number(prompt("Enter your age: "));
+
+if(age>=18){
+    console.log("The person can vote");
+}
+else{
+    console.log("The person cannot vote");
+}
