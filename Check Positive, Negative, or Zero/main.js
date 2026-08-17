75c8ee6302dd03eabc9c025e4558919e0616fe22
samragii <samragineupane7@gmail.com>
2026-08-17 22:00:08 +0545

    Check Positive,Negative or Zero

---
+++ Check Positive, Negative, or Zero/main.js
@@ -0,0 +1,12 @@
+// Get user input
+let number = parseInt(prompt("Enter an integer:"));
+
+if(number>0){
+    console.log("positive")
+}
+else if(number<0){
+    console.log("negative")
+}
+else{
+    console.log("zero")
+}
