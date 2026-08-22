8dc075b7e3c40e8908d1187797410d15c57afedb
samragii <samragineupane7@gmail.com>
2026-08-22 21:25:19 +0545

    Call Function

---
+++ Call Function/main.js
@@ -0,0 +1,15 @@
+
+// Create the sum function
+function sum() {
+    // Get user input
+    let num1 = Number(prompt("Enter a number:"));
+
+    // Calculate sum
+    let num2 = Number(prompt("Enter a number:"));
+
+    // Display the sum
+    console.log(num1+num2);
+}
+
+// Call the sum function
+sum();
