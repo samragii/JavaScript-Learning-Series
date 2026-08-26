493a4ac8fba3e322c96a0c66e5769fa43c786b49
samragii <samragineupane7@gmail.com>
2026-08-26 17:19:08 +0545

    Find Last Element's Index

---
+++ Find Last Element's Index/main.js
@@ -0,0 +1,8 @@
+
+const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "black", "white"];
+
+// Find and store last element's length
+let lastIndex =colors.length-1;
+
+// Display the index of the last element
+console.log(lastIndex);
