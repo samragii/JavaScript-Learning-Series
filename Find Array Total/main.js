73e43630526145e8ba43ee7e7f85f5460e02f164
samragii <samragineupane7@gmail.com>
2026-08-26 17:22:33 +0545

    Find Arry Total

---
+++ Find Array Total/main.js
@@ -0,0 +1,15 @@
+
+const scores = [40, 85, 65, 70, 30];
+
+// Variable to store sum
+
+let total=0
+// Loop through array
+for (let i=0;i<=scores.length-1;i++) {
+
+    // Add score to total
+    total+=scores[i]
+}
+
+// Display total
+console.log(total);
