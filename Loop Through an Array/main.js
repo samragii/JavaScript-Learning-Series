d04da6495b1aed1077fb7eb822104b97d32d0fee
samragii <samragineupane7@gmail.com>
2026-08-26 17:21:01 +0545

    Loop Through an Array

---
+++ Loop Through an Array/main.js
@@ -0,0 +1,13 @@
+
+const scores = [40, 85, 65, 70, 30];
+
+// Store last element's index
+let lastIndex = scores.length-1;
+
+// Loop from last to first index
+for (let i=lastIndex;i>=0;i--) {
+
+    // Display current element
+
+   console.log(scores[i]);
+}
