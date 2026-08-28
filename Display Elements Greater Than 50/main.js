9f4f70628eefff687c9a798ccee23557e317a740
samragii <samragineupane7@gmail.com>
2026-08-28 22:58:54 +0545

    Display Elements Greater than 50

---
+++ Display Elements Greater Than 50/main.js
@@ -0,0 +1,12 @@
+// Array containing scores
+const scores = [40, 85, 65, 70, 30];
+
+// Loop through the array elements
+// Display score if it's greater than 50
+for(let i=0;i<scores.length-1;i++)
+{
+    if(scores[i]>50)
+    {
+        console.log(scores[i]);
+    }
+}
