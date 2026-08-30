94d23b601b5b3f6913bbb20f201e974876774665
samragii <samragineupane7@gmail.com>
2026-08-30 22:44:38 +0545

    Object Operations

---
+++ Object Operations/main.js
@@ -0,0 +1,13 @@
+// Given object
+const prices = {apple: 2.5, kiwi: 3.4};
+
+// Change the value of apple key to 3.5
+prices.apple = 3.5;
+// Display updated prices
+console.log(prices);
+
+// Add an item with banana as key with value 3
+prices.banana=3;
+
+// Display updated prices
+console.log(prices)
