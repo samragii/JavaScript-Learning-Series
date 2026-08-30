2d38aea753a143a7ba1b2e19c55deffa2c3dc350
samragii <samragineupane7@gmail.com>
2026-08-30 22:47:57 +0545

    Access Array Property

---
+++ Access Array Property/main.js
@@ -0,0 +1,13 @@
+// Given object
+const person = {
+    name: "Emma",
+    hobbies: ["dancing","swimming","traveling"]
+};
+
+// Access and print the array
+console.log(person.hobbies);
+// Change the second element to cycling
+person.hobbies[1] ="cycling";
+
+// Access and print the array again
+console.log(person.hobbies);
