37e492538d007c28a8963f60b77791c275d54bed
samragii <samragineupane7@gmail.com>
2026-08-30 22:40:52 +0545

    Access Object Property

---
+++ Access Object Property/main.js
@@ -0,0 +1,11 @@
+// Create an object with two key-value pairs
+const car = {
+    model:"Toyota",
+    year:2022
+};
+
+// Access and display the model property
+console.log(car.model);
+
+// Access and display the year property
+console.log(car.year);
