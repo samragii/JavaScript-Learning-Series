ba81c57093604eceb8293778eaba24f1f512ec26
samragii <samragineupane7@gmail.com>
2026-08-31 20:57:05 +0545

    Find Total Number of Users

---
+++ Find Total Number of Users/main.js
@@ -0,0 +1,12 @@
+// Users data objects
+let users = {
+    user1: "Alice",
+    user2: "Bob",
+    user3: "Charlie"
+};
+
+// Retrieve all the keys
+const newbox = Object.keys(users);
+
+// Display keys length
+console.log(newbox.length);
