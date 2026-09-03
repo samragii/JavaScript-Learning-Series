066bd4150808d82c60e91248656fbf9e7332903c
samragii <samragineupane7@gmail.com>
2026-09-03 22:21:18 +0545

    Remove Unwanted Spaces

---
+++ Remove Unwanted Spaces/main.js
@@ -0,0 +1,9 @@
+// Replace ___ with your code
+
+let text = "          I love coding!          ";
+
+// Remove unwanted spaces
+let trimmedText = text.trim();
+
+// Display new text
+console.log(trimmedText);
