ebab684bd97e6c6cf87061cd9e2e7513809fa1e7
samragii <samragineupane7@gmail.com>
2026-09-03 22:02:31 +0545

    Change Case

---
+++ Change Case/main.js
@@ -0,0 +1,11 @@
+let text1 = "HELLO";
+let text2 = "world";
+
+// Convert text1 to lowercase
+let lowercaseText1 = text1.toLowerCase();
+
+// Convert text2 to uppercase
+let uppercaseText2 = text2.toUpperCase();
+
+// Display both
+console.log(`${lowercaseText1}\n${uppercaseText2}`);
