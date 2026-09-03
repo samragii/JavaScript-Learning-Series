496d872dc08515e92abbd02ae55e39954f87e41c
samragii <samragineupane7@gmail.com>
2026-09-03 22:41:17 +0545

    Replace Substring

---
+++ Replace Substring/main.js
@@ -0,0 +1,9 @@
+// Replace ___ with your code
+
+let text = "I love coding, coding is fun,  coding helps you grow.";
+
+// Replace all occurrences of "coding" with "programming"
+let updatedText = text.replaceAll("coding","programming");
+
+// Display updated text
+console.log(updatedText);
