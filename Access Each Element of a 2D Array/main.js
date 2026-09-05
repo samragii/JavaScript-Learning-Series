aec679ece5e5b7da78474e77560a27876cf1442a
samragii <samragineupane7@gmail.com>
2026-09-05 22:56:57 +0545

    Access Each Element of a 2D Array

---
+++ Access Each Element of a 2D Array/main.js
@@ -0,0 +1,10 @@
+const data = [
+    [2, 3, 5],
+    [7, 14, 21],
+    [1, 3, 5]
+];
+
+// Access and print each element
+for(let i=0;i<data.length;i++)
+  for(let j=0;j<data[i].length;j++)
+    console.log(data[i][j]);
